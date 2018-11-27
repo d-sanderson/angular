62996b993867a9270240b871da2b27d856ff5e8e
dsanderson90 <dsanderson3@cnm.edu>
2018-11-27 14:00:07 -0700

    added splash directory and files

---
+++ src/app/splash/splash.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from "@angular/core";
+import {User} from "../shared/interfaces/user";
+import {UserService} from "../shared/services/user.service";
+
+@Component({
+	template: require("./splash.component.html")
+})
+
+export class SplashComponent implements OnInit {
+	users: User[];
+
+	constructor( protected userService: UserService) {}
+
+
+	ngOnInit(): void {
+
+		this.userServices.getAllUsers()
+			.subscribe(users =>this.users = users);
+
+	}
+
+
+
+
+
+
+}
